037554ebd30437a60489840063b3ad997e507bc5
Sakshi Verma <sakshiverma2523@gmail.com>
2026-06-08 19:15:16 +0530

    Update API base URL to production environment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,14 +1,16 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://mini-social-post-application-niry.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
